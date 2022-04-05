812cf3f13a889b773a57b0929ee43b460faccaf4
Faisal Karim <faisalchy64@gmail.com>
2022-04-05 17:41:36 +0600

    make charts responsive

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import {
     Bar,
     BarChart,
@@ -25,28 +25,24 @@ function Dashboard() {
     return (
         <Container className="my-5">
             <ResponsiveContainer width="95%" height={400}>
-                <Row className="g-4">
-                    <Col lg="6">
-                        <LineChart width={500} height={400} data={data}>
-                            <Line type="monotone" dataKey="Sell"></Line>
-                            <CartesianGrid></CartesianGrid>
-                            <XAxis dataKey="Month"></XAxis>
-                            <YAxis dataKey="Sell"></YAxis>
-                            <Tooltip></Tooltip>
-                        </LineChart>
-                    </Col>
-                    <Col lg="6">
-                        <BarChart width={500} height={400} data={data}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="Month" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="Investment" fill="#8884d8" />
-                            <Bar dataKey="Revenue" fill="#82ca9d" />
-                        </BarChart>
-                    </Col>
-                </Row>
+                <LineChart width={500} height={400} data={data}>
+                    <Line type="monotone" dataKey="Sell"></Line>
+                    <CartesianGrid></CartesianGrid>
+                    <XAxis dataKey="Month"></XAxis>
+                    <YAxis dataKey="Sell"></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </ResponsiveContainer>
+            <ResponsiveContainer className="mt-5" width="95%" height={400}>
+                <BarChart width={500} height={400} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="Month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="Investment" fill="#8884d8" />
+                    <Bar dataKey="Revenue" fill="#82ca9d" />
+                </BarChart>
             </ResponsiveContainer>
         </Container>
     );
